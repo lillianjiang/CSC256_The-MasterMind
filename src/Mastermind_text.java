82da8864213e672d5b38a8c9c3a4687e832d4a26
Lillian <lillianj@smith.edu>
2019-02-27 14:33:22 -0500

    Basic working

---
+++ src/Mastermind_text.java
@@ -0,0 +1,36 @@
+
+public class Mastermind_text {
+	public class board{
+		int row;
+		int col;
+		String Kegs[] = new String[6];
+		//Black/white kegs?
+		
+		/*
+		 * X is user input of how many times they would like to try out.
+		 * Idea: Easy=20 / Mid=15 / Hard=10
+		 * TODO: A bufferreader need to be implement.
+		 */
+//		public board(int x) {
+		public board() {
+			row=11;
+			col=4;
+		//	Kegs[6]= {"R","B","G","Y","P","O"};
+		}
+	}
+	
+//	public Mastermind_text() {
+//		
+//	}
+	public static void main(String[] args) {
+		System.out.print("hello");
+//		Mastermind_text newgame;
+//		for (int i = 0; i < row; i++) {
+//			for (int j = 0; j < col; j++) {
+//				System.out.print(newgame.board[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+	}
+
+}
